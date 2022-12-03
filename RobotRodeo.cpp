7c1d61e52f9f95958183d994b69b379d400e016e
NaveenMP <naveenmanikandanp@gmail.com>
2022-12-03 05:20:21 +0000

    Logic implemented with errors for testcases in main

--- RobotRodeo.cpp
+++ RobotRodeo.cpp
@@ -1,17 +1,176 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <ostream>
 
+using namespace std;
 /*
 *   
+*  INPUT
+*  Vector of strings which contains path commands (Sequences containing G, L, R commands)
 *
-*
-*
-*  Constrains:
+*  OUTPUT
+*  vector of strings with YES or NO for each set of input command indication whether circle/closed-loop pattern is found 
+*  
+*  CONSTRAINTS:
 *  1 <= |commands[i]| <= 2500
 *  1 <= n <= 10
 *  Each command consists of G, L, and R only
 */
 
+vector <string> doesCircleExist(vector<string> commands);
+pair<int, int> operator+=(pair<int, int> &Lobj, const pair<int, int> &Robj);
+pair<int, int> operator+(const pair<int, int> &obj1, const pair<int, int> &obj2);
+bool operator==(const pair<int, int> &Lobj, const pair<int, int> &Robj);
+ostream& operator<<(ostream &os, vector<pair<int, int>> vecIntPair);
+
+int main()
+{
+    vector<string> commands = {"GRGL", "RG"};
+    vector <string> results = doesCircleExist(commands);
+
+    for (auto i = 0; i < results.size(); ++i)
+    {
+        cout << results[i] << endl;
+    }
+
+    return 0;
+}
+
 vector <string> doesCircleExist(vector<string> commands)
 {
+    //Map GLR commands to NEWS commands(string) and coordinates(tuple)
 
+    vector <string> results;
+    results.resize(commands.size());
+
+    for (size_t n = 0; n < commands.size(); ++n)
+    {
+        string commandDirectives = commands[n];
+        string commandResult = "NO";
+        size_t nDirectives = commandDirectives.size();
+        size_t maxDirectives = 2500;
+        vector<pair<int, int>> coordinates;
+        char compass('N');
+
+        for(size_t i=0; i< maxDirectives; ++i)
+        {
+            if (i == 0)
+            {   
+                coordinates.push_back(pair<int, int>(0,0));
+                if (commandDirectives[0] == 'G')
+                {
+                    compass = 'N';
+                }
+                else if(commandDirectives[0] == 'R')
+                {
+                    compass = 'W';
+                }
+                else if(commandDirectives[0] == 'L')
+                {
+                    compass = 'E';
+                }
+            }
+            else
+            {
+                if (commandDirectives[i % nDirectives] == 'G')
+                {
+                    switch(compass)
+                    {
+                        case 'N':
+                            coordinates.push_back(coordinates[i-1]+pair<int, int>(0,1));
+                        case 'E':
+                            coordinates.push_back(coordinates[i-1]+pair<int, int>(-1,0));
+                        case 'W':
+                            coordinates.push_back(coordinates[i-1]+pair<int, int>(1,0));
+                        case 'S':
+                            coordinates.push_back(coordinates[i-1]+pair<int, int>(0,-1));   
+                    }
+                }
+
+                char compassUpdate('N'); 
+                if (commandDirectives[i % nDirectives] == 'L')
+                {
+                    switch(compass)
+                    {
+                        case 'N':
+                            compassUpdate = 'E'; 
+                        case 'E':
+                            compassUpdate = 'S';
+                        case 'W':
+                            compassUpdate = 'N';
+                        case 'S':
+                            compassUpdate = 'W';
+                        
+                    }
+                }
+                else if (commandDirectives[i % nDirectives] == 'R')
+                {
+                    switch(compass)
+                    {
+                        case 'N':
+                            compassUpdate = 'W'; 
+                        case 'E':
+                            compassUpdate = 'N';
+                        case 'W':
+                            compassUpdate = 'S';
+                        case 'S':
+                            compassUpdate = 'E';
+                        
+                    }
+                }
+                compass = compassUpdate;
+
+                if (i%nDirectives == 0 && i>=nDirectives == 0 && coordinates[coordinates.size()-1] == pair<int, int>(0,0))
+                {
+                    commandResult = "YES";
+                    break;
+                }
+            }            
+        }
+
+        cout << coordinates << endl;
+        
+        results[n] = commandResult;
+
+    }
+    return results;
+
+}
+
+pair<int, int> operator+=(pair<int, int> &Lobj, const pair<int, int> &Robj)
+{
+    Lobj.first += Robj.first;
+    Lobj.second += Robj.second;
+    return Lobj;
+}
+
+pair<int, int> operator+(const pair<int, int> &obj1, const pair<int, int> &obj2)
+{
+    pair<int, int> result;
+    result.first  = obj1.first + obj2.first;
+    result.second = obj1.second + obj2.second;
+    return result;
+}
+
+bool operator==(const pair<int, int> &Lobj, const pair<int, int> &Robj)
+{
+    bool res = false;
+    if ((Lobj.first == Robj.first) && (Lobj.second == Robj.second))
+        res = true;
+    
+    return res;
+}
+
+ostream& operator<<(ostream &os, vector<pair<int, int>> vecIntPair)
+{
+    for (auto i=0; i<vecIntPair.size(); ++i)
+    {
+        if (i != vecIntPair.size())
+            os << "(" << vecIntPair[i].first << "," << vecIntPair[i].second << "),";
+        else
+            os << "(" << vecIntPair[i].first << "," << vecIntPair[i].second << endl;
+    }
+    return os; 
 }
