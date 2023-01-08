89cbef914147378415a9ebcf01e8c47160c5756e
NaveenMP <naveenmanikandanp@gmail.com>
2023-01-08 02:39:47 -0500

    Fail at testcase 285 / 353 testcases passed  | s = aasdfasdfasdfasdfas p = aasdf.*asdf.*asdf.*asdf.*s Output false Expected true

--- RegExpMatching.cpp
+++ RegExpMatching.cpp
@@ -24,87 +24,117 @@ public:
     bool isMatch(string s, string p)
     {
         size_t pos = 0;
-        if (p.length() > s.length()) // prune to match sizes
-        {
-            while (pos++ < p.length())
-            {
-                if ((p[pos] == '*') && (s.find(p[pos-1]) > s.length()))
-                {
-                    p.erase(pos-1,2);
-                    pos = 0;
-                }
-            }
 
-            pos = 0;
-            while(p.length() > s.length())
-            {
-                if (p[pos] != s[pos])
-                {
-                    p.erase(pos,1);
-                }
-                else if (p[p.length()-1] != s[s.length()-1] && p[p.length()-1] != '*')
-                {
-                    p.erase(p.length()-1, 1);
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            size_t lDiff = 0;
-            while (p.length() > s.length() && (pos++ < s.length()))
-            {
-                if (p[pos] == '*' && (s.find(p[pos-1]) < s.length()))
-                {
-                    lDiff = s.length() - p.length();
-                    if (lDiff > 0)
-                    {
-                        p.erase(pos,1);
-                        pos = 0;
-                    }
-                }
-            }
-
-        }
-
-        if (s.length() >= p.length())
+        while(p.find('*') < p.length() || pos==0)
         {
             if (p == ".*")
-                return true;
+                    return true;
 
-            size_t pos = 0;
-            while (pos < s.length())
+            if (p.length() > s.length()) // prune to match sizes
             {
-                if(p[pos] == s[pos])
+                while (pos++ < p.length())
                 {
-                    ++pos;
-                    continue;
-                }
-                else if (p[pos] == '*' && pos > 0)
-                {
-                    p[pos] = p[pos-1];
-                    if (pos+1 < s.length())
+                    if ((p[pos] == '*') && (s.find(p[pos-1]) > s.length()))
                     {
-                        if ((p[pos+1] != s[pos+1]) && (p[pos] == s[pos+1]) && p.length() < s.length())
+                        p.erase(pos-1,2);
+                        pos = 0;
+                    }
+                    
+                    if(p[pos] == '.')
+                    {
+                        if (p[pos+1] == '*'){
+                            p.erase(pos,2);
+                        }
+                        else
                         {
-                            p.insert(pos+1, 1, '*');
-                            ++pos;
-                            continue;
+                            if (pos > s.length()-1)
+                                p[pos] = s[s.length()-1];
+                            else
+                                p[pos] = s[pos];
                         }
                     }
                 }
-                else if (p[pos] == '.')
-                {
-                    p[pos] = s[pos];
-                }
-                else  // non-matching character
-                {
-                    return false;
-                }
-                
-                ++pos;
 
+                pos = 0;
+                while(p.length() > s.length())
+                {
+                    if (p[pos] != s[pos])
+                    {
+                        p.erase(pos,1);
+                    }
+                    else if (p[p.length()-1] != s[s.length()-1] && p[p.length()-1] != '*')
+                    {
+                        p.erase(p.length()-1, 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                size_t lDiff = 0;
+                while (p.length() > s.length() && (pos++ < p.length())) //(pos++ < s.length())
+                {
+                    if (p[pos] == '*' && (s.find(p[pos-1]) < s.length()))
+                    {
+                        lDiff = p.length() - s.length();
+                        if (lDiff > 0)
+                        {
+                            if (p[pos-1]!=s[pos-1])
+                                p.erase(pos-1, lDiff); //p.erase(pos,1);
+                            else
+                                if (count(p.begin(),p.end(),'*')==1)
+                                    p.erase(pos, lDiff);
+                                else
+                                    p.erase(pos, 1);
+                            //pos = 0;
+                        }
+                    }
+                }
+
+                if (s == p)
+                    return true;
+
+            }
+
+            if (s.length() >= p.length())
+            {
+                if (s == p)
+                    return true;
+
+                size_t pos = 0;
+                while (pos < s.length())
+                {
+                    if(p[pos] == s[pos])
+                    {
+                        ++pos;
+                        continue;
+                    }
+                    else if (p[pos] == '*' && pos > 0)
+                    {
+                        p[pos] = p[pos-1];
+                        if (pos+1 < s.length())
+                        {
+                            if ((p[pos+1] != s[pos+1]) && (p[pos] == s[pos+1])) //&& (p.length() < s.length())
+                            {
+                                p.insert(pos+1, 1, '*');
+                                ++pos;
+                                continue;
+                            }
+                        }
+                    }
+                    else if (p[pos] == '.')
+                    {
+                        p[pos] = s[pos];
+                    }
+                    else  // non-matching character
+                    {
+                        return false;
+                    }
+                    
+                    ++pos;
+
+                }
             }
         }
 
@@ -124,18 +154,33 @@ int main()
                                 // mississp*.
                                 // mississpp.
                                 // mississppi
-    // Failed at test case 169/353 | s = "aab"   p = "c*a*b"   ; exp: true
+    // Failed at test case 169/353 | s = "aab"   p = "c*a*b"    ; exp: true
 
-    // Failed at test case 275/353 | s = "aaa"   p = "aaaa"    ; Output: true  Expected: false
+    // Failed at test case 275/353 | s = "aaa"   p = "aaaa"     ; Output: true  Expected: false
 
-    // Failed at test case 277/353 | s = "aaa"   p = "ab*ac*a" ; Output: false Expected: true
+    // Failed at test case 277/353 | s = "aaa"   p = "ab*ac*a"  ; Output: false Expected: true
 
-    // Failed at test case 281/353 | s = "aaa"   p = "ab*a*c*a"; Output: false Expected: true
+    // Failed at test case 280/353 | s = "aaca"  p = "ab*a*c*a" ; Output: false Expected: true (detected after 285/353)
 
-    // Failed at test case 285/353 | s = "bbbba" p = ".*a*a"   ; Output: false Expected: true
+    // Failed at test case 281/353 | s = "aaa"   p = "ab*a*c*a" ; Output: false Expected: true
+
+    // Failed at test case 284/353 | s = "a"     p =".*"        ; Output: false Expected: true
+
+    // Failed at test case 285/353 | s = "bbbba" p = ".*a*a"    ; Output: false Expected: true
+
+    // Failed at test case 285/353 | s = "a"     p =".*..a*"    ; Output: true  Expected: false
+
+    // 285 / 353 testcases passed  | s = "aasdfasdfasdfasdfas" p = "aasdf.*asdf.*asdf.*asdf.*s" Output false Expected true
+
 
     Solution S;
-    bool ret = S.isMatch("bbbba", ".*a*a");
+    clock_t start = clock();
+    bool ret = S.isMatch("aasdfasdfasdfasdfas", "aasdf.*asdf.*asdf.*asdf.*s");
+    double elapsedSecs = (clock() - start) / ((double)CLOCKS_PER_SEC);
+    double elapsedMilliSecs = elapsedSecs*1000;
     cout << ret << endl;
+    
+    cout << "---------- SUMMARY ----------" << endl;
+    cout << "Elapsed time : " << elapsedMilliSecs << "ms" << endl;
     return 0;
 }
