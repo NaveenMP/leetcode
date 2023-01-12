515a023232a833e37d06b2fd5820a60014ab639f
NaveenMP <naveenmanikandanp@gmail.com>
2023-01-12 17:28:06 -0500

    Added logic for edge case where len(p)>len(s) s = aaa, p = ab*a*c*a

--- RegExpMatching.cpp
+++ RegExpMatching.cpp
@@ -90,11 +90,17 @@ public:
             {
                 if (s[s_idx] == p[p_idx-1])
                 {
-                    if (s[s_idx+1] == s[s_idx])
+                    if (s[s_idx+1] == s[s_idx]){
                         p.insert(p_idx, 1, p[p_idx-1]);
-                    else
-                        p[p_idx] = p[p_idx-1];
-
+                    }
+                    else{
+                        if (s_idx < s_len-1)
+                            p[p_idx] = p[p_idx-1];
+                        else{
+                            p.erase(p_idx,1);
+                            continue;
+                        }
+                    }
                     ++p_idx;
                     ++s_idx;
                 }
@@ -169,184 +175,3 @@ int main()
     cout << "Elapsed time : " << elapsedMilliSecs << "ms" << endl;
     return 0;
 }
-
-
-
-/*
-int main () {
-  
-  string s = "aasdfasdfasdfasdfas"; 
-  string p = "aasdf.*asdf.*asdf.*asdf.*s";
-  //cout << "s = " << s << endl << "p = " << p << endl;
-  
-  size_t posPdotStar = 0; 
-  size_t posSpreDotStarMatch = 0;
-  
-  while (p.find(".*") != string::npos)
-  {
-      posPdotStar = p.find(".*"); 
-      posSpreDotStarMatch = s.find(p.substr(0,posPdotStar-1));
-      
-      cout << "s = " << s << endl << "p = " << p << endl << endl;
-      
-      cout << "pos .*            :: " << posPdotStar << endl;
-      cout << "pos pre (in S) .* :: " << posSpreDotStarMatch << endl;
-      
-      if (posSpreDotStarMatch!=string::npos)
-      {
-         s.erase(posSpreDotStarMatch, posPdotStar);
-         if (s[0] == p[posPdotStar+2])
-         {
-            p.erase(posSpreDotStarMatch, posPdotStar+2);
-         }
-         else
-         {
-           p.erase(posSpreDotStarMatch, posPdotStar);
-           p.insert(0,1,s[0]);
-         }
-      }
-      else
-      {
-          break;
-      }
-      
-      cout << endl;
-  }
-  
-  cout << "s = " << s << endl << "p = " << p << endl << endl;
-  
-  return 0;
-}
-*/
-
-
-/*
-class Solution
-{
-public:
-    bool isMatch(string s, string p)
-    {
-        size_t pos = 0;
-
-        while(p.find('*') < p.length() || pos==0)
-        {
-            if (p == ".*")
-                    return true;
-
-            if (p.length() > s.length()) // prune to match sizes
-            {
-                while (pos++ < p.length())
-                {
-                    if ((p[pos] == '*') && (s.find(p[pos-1]) > s.length()))
-                    {
-                        p.erase(pos-1,2);
-                        pos = 0;
-                    }
-                    
-                    if(p[pos] == '.')
-                    {
-                        if (p[pos+1] == '*' && p[pos+2] == s[pos])
-                        {
-                            p.erase(pos,2);
-                        }
-                        else
-                        {
-                            if (pos > s.length()-1)
-                                p[pos] = s[s.length()-1];
-                            else
-                                p[pos] = s[pos];
-                        }
-                    }
-                }
-
-                pos = 0;
-                while(p.length() > s.length())
-                {
-                    if (p[pos] != s[pos])
-                    {
-                        p.erase(pos,1);
-                    }
-                    else if (p[p.length()-1] != s[s.length()-1] && p[p.length()-1] != '*')
-                    {
-                        p.erase(p.length()-1, 1);
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                size_t lDiff = 0;
-                while (p.length() > s.length() && (pos++ < p.length())) //(pos++ < s.length())
-                {
-                    if (p[pos] == '*' && (s.find(p[pos-1]) < s.length()))
-                    {
-                        lDiff = p.length() - s.length();
-                        if (lDiff > 0)
-                        {
-                            if (p[pos-1]!=s[pos-1])
-                                p.erase(pos-1, lDiff); //p.erase(pos,1);
-                            else
-                                if (count(p.begin(),p.end(),'*')==1)
-                                    p.erase(pos, lDiff);
-                                else
-                                    p.erase(pos, 1);
-                            //pos = 0;
-                        }
-                    }
-                }
-
-                if (s == p)
-                    return true;
-
-            }
-
-            if (s.length() >= p.length())
-            {
-                if (s == p)
-                    return true;
-
-                size_t pos = 0;
-                while (pos < s.length())
-                {
-                    if(p[pos] == s[pos])
-                    {
-                        ++pos;
-                        continue;
-                    }
-                    else if (p[pos] == '*' && pos > 0)
-                    {
-                        p[pos] = p[pos-1];
-                        if (pos+1 < s.length())
-                        {
-                            if ((p[pos+1] != s[pos+1]) && (p[pos] == s[pos+1])) //&& (p.length() < s.length())
-                            {
-                                p.insert(pos+1, 1, '*');
-                                ++pos;
-                                continue;
-                            }
-                        }
-                    }
-                    else if (p[pos] == '.')
-                    {
-                        p[pos] = s[pos];
-                    }
-                    else  // non-matching character
-                    {
-                        return false;
-                    }
-                    
-                    ++pos;
-
-                }
-            }
-        }
-
-        if (s == p)
-            return true;
-        else
-            return false;
-    }
-};
-
-*/
